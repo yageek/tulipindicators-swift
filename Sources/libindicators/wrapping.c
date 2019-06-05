d6507c5361d21bc96bd2fc9b60afe78b5848fbb3
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-06-05 15:21:39 +0200

    Update LICENSE to C file

--- Sources/libindicators/wrapping.c
+++ Sources/libindicators/wrapping.c
@@ -1,3 +1,26 @@
+/*
+ * tulipindicators-swfit
+ * https://tulipindicators.org/
+ * Copyright (c) 2019 Yannick Heinrich
+ * Yannick Heinrich (me@yageek.net)
+ *
+ * This file is part of tulipindicators-swfit.
+ *
+ * Tulip Indicators is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Tulip Indicators is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Tulip Indicators.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "indicators.h"
 
 struct ti_indicator_info *ti_indicators_ptr = &ti_indicators[0];
